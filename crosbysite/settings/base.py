647a7bf795c2e39d68b0030192b40060f16e1142
John Crosby <johncrosby36@gmail.com>
2025-01-12 12:10:57 +0000

    added csrf

--- crosbysite/settings/base.py
+++ crosbysite/settings/base.py
@@ -178,3 +178,7 @@ WAGTAILADMIN_BASE_URL = "http://example.com"
 # if untrusted users are allowed to upload files -
 # see https://docs.wagtail.org/en/stable/advanced_topics/deploying.html#user-uploaded-files
 WAGTAILDOCS_EXTENSIONS = ['csv', 'docx', 'key', 'odt', 'pdf', 'pptx', 'rtf', 'txt', 'xlsx', 'zip']
+
+CSRF_TRUSTED_ORIGINS = [
+    'https://crosby-estates-wagtail-stage.eu.aldryn.io',
+]
